96c8b1951434857f2d19d789514af859a82092f4
Rorsharck <yadunandanet@gmail.com>
2024-05-03 14:09:23 +0200

    Flipping and contrast control

--- scripts/process_caleb_pipeline.py
+++ scripts/process_caleb_pipeline.py
@@ -2,7 +2,7 @@ from multicam_calibration.detection import process_video, run_calibration_detect
 from pathlib import Path
 
 
-input_folder = Path('/Users/vigji/Desktop/rig_test_videos/')
+input_folder = Path(r'C:\Users\Administrator\Desktop\rig_test_videos')
 all_crop_files = list(input_folder.glob('checkerboard*/*/*.avi'))
 assert len(all_crop_files) > 0, "No video files found in the specified folder"
 
@@ -13,4 +13,5 @@ all_videos = [str(crop_file) for crop_file in all_crop_files]
 # print(all_videos)
 if __name__ == '__main__':
     run_calibration_detection(all_videos, detect_chessboard, 
-                          detection_options=dict(board_shape=(5, 7)), n_workers=10)
+                          detection_options=dict(board_shape=(5, 7)), n_workers=12)
+    
