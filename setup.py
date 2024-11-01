fa837ea32abb492c3cfc6540ce1fd63f84509216
Luigi Petrucco <luigi.petrucco@gmail.com>
2024-11-01 10:03:06 +0100

    setup fix for tests

--- setup.py
+++ setup.py
@@ -3,20 +3,23 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open("requirements_dev.txt") as f:
+    requirements_dev = f.read().splitlines()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="threed_utils",
     version="0.0.1",
     author="Iurilli lab",
-    include_package_data=True,
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_namespace_packages(exclude=("docs", "tests*")),
+    install_requires=requirements,
+    extras_require=dict(dev=requirements_dev),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
-    install_requires=[
-        "tqdm",
-        "matplotlib",
-    ],
     url="https://github.com/iurillilab/3d-setup",
 )
