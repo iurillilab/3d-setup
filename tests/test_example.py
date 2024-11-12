61746fbdfa6bcbabe0353a76e0fbf4382cab1a25
Luigi Petrucco <luigi.petrucco@gmail.com>
2024-11-12 21:43:50 +0100

    tests running

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,6 @@
 import pytest
 
-import threed_utils as tdu
+import threed_utils.visualization as tdv
 
 
 @pytest.mark.parametrize("temp_dir", ["temp_mouse_data_dir", "temp_calib_data_dir"])
@@ -23,19 +23,19 @@ def test_temp_dir_contains_expected_files(request, temp_dir: str):
     ), f"Temporary data directory {temp_dir} does not contain the expected number of files."
 
 
-def test_video_processing():
-    video_location = "tests/data/test_video.mp4"
+# def test_video_processing():
+#     video_location = "tests/data/test_video.mp4"
 
-    cropped_video_dict = crop_video(video_location)
+#     cropped_video_dict = crop_video(video_location)
 
-    assert cropped_video_dict.keys() == ["left", "right", "top", "bottom"]
+#     assert cropped_video_dict.keys() == ["left", "right", "top", "bottom"]
 
-    assert cropped_video_dict["left"].shape == (10, 10)
+#     assert cropped_video_dict["left"].shape == (10, 10)
 
-    assert np.allequal(
-        cropped_video_dict["left"][0, :10], np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    )
+#     assert np.allequal(
+#         cropped_video_dict["left"][0, :10], np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+#     )
 
 
 def test_helloworld():
-    assert tdu.visualization.test_helloworld() == "the usual"
+    assert tdv.test_helloworld() == "the usual"
