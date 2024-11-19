c8da31e59a2ff90c6b4db568f9cead1e249a94b8
Luigi Petrucco <luigi.petrucco@gmail.com>
2024-11-19 14:37:37 +0100

    script for dlc inference

---
+++ scripts/dlc/run_dlc_inference.py
@@ -0,0 +1,35 @@
+import deeplabcut
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description="Run DLC inference on videos")
+parser.add_argument(
+    "--videos_dir",
+    type=str,
+    required=True,
+    help="Path to directory containing videos",
+)
+parser.add_argument(
+    "--config_file",
+    type=str,
+    required=True,
+    help="Path to DLC config.yaml file",
+)
+parser.add_argument(
+    "--make_labeled_video",
+    action="store_true",
+    default=True,
+    help="Create labeled video output",
+)
+
+args = parser.parse_args()
+
+videos_dir = Path(args.videos_dir)
+path_config_file = args.config_file
+videos_pattern_to_match = "*.avi"
+
+videofile_path = [str(filename) for filename in (videos_dir.glob(videos_pattern_to_match))]
+deeplabcut.analyze_videos(path_config_file, videofile_path)
+
+if args.make_labeled_video:
+    deeplabcut.create_labeled_video(path_config_file, videofile_path)
