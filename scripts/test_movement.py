7de7757f81aa412ebfc330dbe7855da43cf6fa67
Luigi Petrucco <luigi.petrucco@gmail.com>
2024-11-20 21:22:29 +0100

    more tests on movement

--- scripts/test_movement.py
+++ scripts/test_movement.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import pickle
 import numpy as np
-from movement.io import load_poses
+from movement.io import load_poses, save_poses
 
 from threed_utils.triangulation import triangulate_all_keypoints
 
@@ -23,4 +23,29 @@ triangulated_points = triangulate_all_keypoints(calibration["points"]["tracked_p
 ds = load_poses.from_numpy(np.transpose(triangulated_points, (1, 0, 2))[:, None, :, :])
 
 
+# %%
+# Path with multiple recordings:
+data_path = Path("/Users/vigji/Desktop/test_movement")
+all_files = list(data_path.glob("*.slp"))
+print(all_files)
+ds = load_poses.from_file(all_files[0], source_software="SLEAP")
+# %%
+# take firxt 100 timepoints of xarray dataset:
+ds = ds.isel(time=slice(100))
+ds
+# %%
+view_labels = "mirror-left", "mirror-right", "mirror-top"
+files_dict = {view_label: next(data_path.glob(f"*{view_label}*.slp")) for view_label in view_labels}
+files_dict
+ds = load_poses.from_multi_view_files(files_dict, source_software="SLEAP")
+
+# %%
+from movement.kinematics import compute_displacement, compute_velocity
+compute_velocity(ds)
+# %%
+for file in all_files:
+    ds = load_poses.from_file(file, source_software="SLEAP")
+    save_poses.to_dlc_file(ds.isel(time=slice(200)), file.parent / "testing_dataset" / (file.stem + "_200.h5"))
+# %%
+ds
 # %%
