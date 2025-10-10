736977e7aebde7d262358612801517bf433e047e
rorsharck <yadunandanet@gmail.com>
2025-10-10 10:58:44 +0200

    Local updates

---
+++ scripts/dlc/playground.py
@@ -0,0 +1,7 @@
+#%%
+import pandas as pd
+
+obj = pd.read_pickle('/mnt/nas/SNeuroBiology_shared/CHOMP/20250804/Cricket/M37/091908/multicam_video_2025-08-04T10_11_56_cropped-v2_20251001185918/multicam_video_2025-08-04T10_11_56_centralDLC_HrnetW48_mouse-bottomJul1shuffle1_snapshot_best-189_full.pickle')
+# %%
+obj
+# %%
