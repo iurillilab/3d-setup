28bd4193ede054c0eb97a8c6ea9376588d570296
thomas bush <thomasbush52@gmail.com>
2025-11-09 22:22:45 +0100

    egocentric coordinates func

--- threed_utils/helpers.py
+++ threed_utils/helpers.py
@@ -110,3 +110,62 @@ def arena_filtering(
     session["position"] = pos.where(inside, np.nan)
 
     return session
+
+
+def normalize_ax(v, axis=1, eps=1e-8):
+    norm = np.linalg.norm(v, axis=axis, keepdims=True)
+    return v / np.clip(norm, eps, None)
+
+
+def convert_session_to_egocentric(session: xr.Dataset):
+    session_copy = session.copy()
+    trunk_names = [
+        "back_caudal",
+        "back_mid",
+        "back_rostral",
+        "belly_caudal",
+        "belly_rostral",
+    ]
+    origin = session.position.sel(keypoints=trunk_names).mean(dim="keypoints")
+    # define the body axes:
+    nose = session.position.sel(keypoints="nose").values
+    tailbase = session.position.sel(keypoints="tailbase").values
+    ear_lf = session.position.sel(keypoints="ear_lf").values
+    ear_rt = session.position.sel(keypoints="ear_rt").values
+    origin_np = origin.values
+    pose_np = session.position.values
+
+    T, C, K, A = pose_np.shape
+
+    # forward ax
+    forward = nose - tailbase
+    forward = normalize_ax(forward, axis=1)
+    # left ax
+    left = ear_lf - ear_rt
+    left = normalize_ax(left, axis=1)
+    # up ax
+    f = forward[..., 0]
+    l = left[..., 0]
+    # up= forwar X left (right hand rule)
+    up = np.cross(f, l, axis=1)
+    up = normalize_ax(up, axis=1)
+    # recompute hte left for orthogonality
+    left_2 = np.cross(up, f, axis=1)
+    left_2 = normalize_ax(left_2, axis=1)
+
+    ex = f
+    ey = left_2
+    ez = up
+
+    # build the rotation matrices
+    R = np.stack([ex, ey, ez], axis=2)
+    R_T = np.transpose(R, (0, 2, 1))
+
+    # transfrom the keypoints to egocentric
+    centered = pose_np - origin_np[:, :, None, :]  # broadcast over kp
+    centered = centered[..., 0]
+    pose_ego = np.einsum(
+        "tij, tjk -> tik", R_T, centered
+    )  # we rotate each frame p_body[t] = R_T[t] @ centered[t]
+    session_copy["position"].values = np.expand_dims(pose_ego, axis=-1)
+    return session_copy
